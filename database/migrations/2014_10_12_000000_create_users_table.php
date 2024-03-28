aeecfbdb29e11cc74a56c59bbf4ede3bc18e87f2
xnetta92 <palanett92@gmail.com>
2024-03-28 14:01:14 +0100

    register login

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('firt_name');
+            $table->string('first_name');
             $table->string('last_name');
             $table->string('address');
             $table->string('phone');
--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,6 @@ use Illuminate\Support\Facades\Route;
 # });
 # 
 
-Route::put('login', 'App\Http\Controllers\AuthController@login');
-Route::put('register', 'App\Http\Controllers\AuthController@register');
+Route::post('login', 'App\Http\Controllers\AuthController@login');
+Route::post('register', 'App\Http\Controllers\AuthController@register');
 
